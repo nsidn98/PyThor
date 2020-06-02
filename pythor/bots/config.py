3333f21d0d0bb2646a21f2673ad72f61661dd55f
Siddharth Nayak <siddharthnayak98@gmail.com>
2020-06-02 13:06:14 +0530

    add config for telegrad

---
+++ pythor/bots/config.py
@@ -0,0 +1,7 @@
+
+telegram_token = "YOUR_TOKEN"  # replace TOKEN with your bot's token
+
+#  user id is optional, however highly recommended as it limits the access to you alone.
+telegram_user_id = YOUR_USER_ID  # replace None with your telegram user id (integer)
+
+telegram_config = {'token':telegram_token, 'user_id':telegram_user_id}
